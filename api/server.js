9361837b3e0f7ed3dd0bb6f6faa5377e007373c6
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-07-06 21:03:49 -0500

    Server up and running

--- api/server.js
+++ api/server.js
@@ -1,2 +1,13 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+//const router = require()
+const server = express();
+
+server.use(express.json());
+
+//server.use()
+
+//OTHER ENDPOINTS
+
+module.exports = server;
